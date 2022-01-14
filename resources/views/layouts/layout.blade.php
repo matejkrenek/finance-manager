99f0a7a36c904c7270514c7f250d5bae0f6224e8
matejkrenek <mate23.krenek@gmail.com>
2022-01-14 20:56:34 +0100

    start working on frontend

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,49 +4,35 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <script src="{{ asset('js/index.js') }}" defer></script>
+    <title>{{ config('app.name') }}</title>
 </head>
-    <ul class="flex items-center justify-end pt-4 pr-2">
-        @if (!Auth::user())
-            <li class="ml-2 mr-2">
-                <a href="{{ route('login') }}">Login</a>
-            </li>
-            <li class="ml-2 mr-2">
-                <a href="{{ route('register') }}">Register</a>
-            </li>
-        @else
-            <li class="ml-2 mr-2">
-                <a href="{{ route('notifications') }}">Notifications</a>
-            </li>
-            <li class="ml-2 mr-2">
-                <a href="{{ route('workspace.index') }}">Workspaces</a>
-            </li>
-            <li class="ml-2 mr-2">
-                <a href="{{ route('workspace.create') }}">Create workspace</a>
-            </li>
-            <li class="ml-2 mr-2">
-                <a href="{{ route('dashboard') }}">Dashboard</a>
-            </li>
-            <li class="ml-2 mr-2">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit">Logout</button>
-                </form>
-            </li>
-        @endif
-    </ul>
-    <main style="height: 100vh">
-        {{ $slot }}
-    </main>
+<body>
+    <div id="root">
+        <div class="container pt-64">
+            <aside class="flex-column pr-24 w-17">
+                <div class="square mb-56">
+                    <img src="https://img.freepik.com/free-vector/mouse-mascot-logo-design_317408-45.jpg?size=338&ext=jpg" alt="">
+                </div>
+                <nav class="flex-grow pb-32">
+                    <ul class="flex-column h-100">
+                        <li><a href="" class="link py-8 is-active">Home</a></li>
+                        <li><a href="" class="link py-8">Members</a></li>
+                        <li><a href="" class="link py-8">Settings</a></li>
+                        <li class="flex align-end flex-grow"><a href="" class="link py-8">Log out</a></li>
+                    </ul>
+                </nav>
+            </aside>
+            <div class="w-100">
+                <header>
+    
+                </header>
+                <main>
+                    {{ $slot }}
+                </main>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,9 @@
 const mix = require('laravel-mix');
+const globImporter = require('node-sass-glob-importer');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+mix.js('resources/js/index.js', 'public/js')
+   .sass('resources/scss/main.scss', 'public/css', {
+      sassOptions: {
+         importer: globImporter()
+      }
+   })
